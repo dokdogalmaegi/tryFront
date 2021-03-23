13232efb66e96923a346ef282fd356aa761ba0b4
임준영 <clik202012@gmail.com>
2021-03-24 00:31:41 +0900

    add form component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/ToDoListTemplate.jsx';
+import Form from './components/Form.jsx';
 
 class App extends Component {
-    render() { 
-      return (
-        <TodoListTemplate>
-          템플릿 완성
-        </TodoListTemplate>
-      );
-    }
+  render() {
+    return (
+      <TodoListTemplate form={<Form />}>템플릿 완성</TodoListTemplate>
+    );
+  }
 }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import './From.css';
+import './Form.css';
+
+// this components annotation
+
+/*
+value: content of input
+onCreate: When clicked of button a function is execute
+onChange: When changed of input content a function is execute
+onkeyPress: When input of key a function is execute
+*/
 
 const Form = ({ value, onchange, onCreate, onKeyPress }) => {
   return (
     <div className='form'>
       <input value={value} onChange={onchange} onKeyPress={onKeyPress} />
-      
+      <div className='create-button' onClick={onCreate}>
+        추가
+      </div>
     </div>
-  )
+  );
 };
+
+export default Form;
